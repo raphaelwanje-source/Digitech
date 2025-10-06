fa5ec1a0bb88cb5482aca511b3b5288a92726cd4
raphaelwanje <raphael.wanje@gmail.com>
2025-10-06 12:13:33 +0300

    updates on day 4

---
+++ Digitech_python/day4.py
@@ -0,0 +1,23 @@
+#  this programme checks on the user inputs and validate it 
+print ("this programme needs the credentials to verify them to give grant access to users")
+
+username = "rapahael"
+password = "raphael2025"
+
+if username == "raphael" and password == "raphael2025":
+    print("signed in succesfully!!")
+
+else:
+    print("wrong credentials")
+
+
+username = input("Enter your username")
+password = input("Enter your password")
+
+if username == "raphael" and password == "raphael2025":
+    print("signed in succesfully!!")
+elif username != "raphael" and password == "raphael2025":
+print("check your credentials and try again!!") 
+
+else:
+    print("Wrong credentials")
