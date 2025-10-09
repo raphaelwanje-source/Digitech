570655a4149dc7cec92e8dfdae3572b24965cbc0
raphaelwanje <raphael.wanje@gmail.com>
2025-10-09 12:04:55 +0300

    updates

---
+++ Digitech_python/day6.py
@@ -0,0 +1,24 @@
+a = 5
+b = 4
+calc = a & b
+print(calc)
+
+
+
+a = 5
+b = 4
+calc = a & b
+binary = bin(calc)
+print(f"{calc} and it's binary is {binary}")
+
+a = 8
+b = 6
+calc = a & b
+binary = bin(calc)
+print(f"{calc} and it's binary is {binary}")
+
+a = 10
+b = 8
+calc = a ^ b
+binary = bin(calc)
+print(f"{calc} and it's binary is {binary}")
