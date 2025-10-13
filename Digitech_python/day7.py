c342e243fd10215ba57a8f925c3d895d9c3faafc
raphaelwanje <raphael.wanje@gmail.com>
2025-10-13 12:42:25 +0300

    updates

---
+++ Digitech_python/day7.py
@@ -0,0 +1,30 @@
+a = 5
+print(~a)
+# // -6
+
+# 2's compliment formular
+# 2's = 1's + 1
+
+# logic of 2's operation
+# 6 = 0110
+# ~6 = 1001
+
+# 1's compliment = 1001
+# 2's compliment = 1's + 1 = 1001+1
+
+
+a = 4
+print(~a)
+
+# left shift:
+# hart formular
+# a = 5
+# a << 2 
+# =a x 2^2 =5 x 2^2
+# =5 x 4 = 20 
+
+a = 5
+print(a<<2)
+
+a = 5
+print(a>>2)
